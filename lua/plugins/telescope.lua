8024ed27214e649d7bcddbf4da232736ada3635f
Callum Burns <Callum.Burns@towerswatson.com>
2025-11-10 16:23:52 +0000

    autopairs keys and telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,55 +1,56 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"BurntSushi/ripgrep",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
-	config = function()
-		require("telescope").setup({
-			defaults = {
-				vimgrep_arguments = {
-					"rg",
-					"--no-heading",
-					"--with-filename",
-					"--line-number",
-					"--column",
-					"--smart-case",
-				},
-				file_ignore_patterns = { "node_modules", "build", "dist" },
-				mappings = {
-					i = {
-						["<C-u>"] = false,
-					},
-				},
-			},
-			pickers = {
-				file_files = {
-					hidden = true,
-				},
-			},
-			extensions = {
-				fzf = {
-					fuzzy = true,
-					override_generic_sorter = true,
-					override_file_sorter = true,
-					case_mode = "smart_case",
-				},
-			},
-		})
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "BurntSushi/ripgrep",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+        },
+        file_ignore_patterns = { "node_modules", "build", "dist" },
+        mappings = {
+          i = {
+            ["<C-u>"] = false,
+          },
+        },
+      },
+      pickers = {
+        file_files = {
+          hidden = true,
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+      },
+      path_display = { "truncate" },
+    })
 
-		require("telescope").load_extension("fzf")
+    require("telescope").load_extension("fzf")
 
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
-		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Key Maps" })
-		vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Quickfix List" })
-		vim.keymap.set("n", "<leader>fc", function()
-			builtin.find_files({ cwd = "~/AppData/Local/nvim" })
-		end, { desc = "Config Files" })
-	end,
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
+    vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Key Maps" })
+    vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Quickfix List" })
+    vim.keymap.set("n", "<leader>fc", function()
+      builtin.find_files({ cwd = "~/AppData/Local/nvim" })
+    end, { desc = "Config Files" })
+  end,
 }

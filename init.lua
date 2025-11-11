a9300cd68787ca8952841aba815fb80ff4c5e9a6
cagburns <callum.burns0131@gmail.com>
2025-11-11 01:03:28 +0000

    Cleaup auto func for shada files

--- init.lua
+++ init.lua
@@ -7,3 +7,32 @@ require("config.autocmds")
 require("config.lazy")
 
 vim.cmd([[colorscheme kanagawa-dragon]])
+
+-- Auto-clean old main.shada.tmp.* files on startup (Windows-safe)
+local shada_dir = vim.fn.stdpath("data") .. "\\shada"
+local max_age_days = 3 -- delete files older than this
+
+local function clean_old_shada_temps()
+	local handle = vim.loop.fs_scandir(shada_dir)
+	if not handle then
+		return
+	end
+
+	local entry = vim.loop.fs_scandir_next(handle)
+	while entry do
+		if entry:match("^main%.shada%.tmp%.") then
+			local fullpath = shada_dir .. "\\" .. entry
+			local stat = vim.loop.fs_stat(fullpath)
+			if stat then
+				local age_days = (os.time() - stat.mtime.sec) / 86400
+				if age_days > max_age_days then
+					os.remove(fullpath)
+				end
+			end
+		end
+		entry = vim.loop.fs_scandir_next(handle)
+	end
+end
+
+-- Run once on startup
+pcall(clean_old_shada_temps)

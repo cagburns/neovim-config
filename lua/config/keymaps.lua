29a1cb4509940234cec3e61180df04e4bde52999
cagburns <callum.burns0131@gmail.com>
2026-01-23 07:35:06 +0000

    Add neotree and update keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,15 @@
 -- Views/Sidebars
-vim.keymap.set("n", "<leader>ee", ":Oil<CR>", { desc = "Toggle File Explorer" })
+vim.keymap.set("n", "<leader>fe", ":Oil<CR>", { desc = "Toggle File Explorer" })
 vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "Open Git Status" })
 vim.keymap.set("n", "<leader>vo", ":copen<CR>", { desc = "Toggle Quickfix" })
 vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>", { desc = "Toggle Terminal" }) -- Requires toggleterm.nvim
+vim.keymap.set("n", "\\", ":Neotree toggle<CR>", { desc = "Toggle Neotree" }) -- Requires neotree.nvim
 -- vim.keymap.set("n", "<leader>vd", ":lua require'dap'.toggle_breakpoint()<CR>", { desc = "Toggle Debugger" }) -- Requires nvim-dap
 
 -- Editor Management
-vim.keymap.set("n", "<leader>ww", ":w<CR>", { desc = "Save File" })
-vim.keymap.set("n", "<leader>wa", ":wa<CR>", { desc = "Save All Files" })
-vim.keymap.set("n", "<leader>wq", ":wq<CR>", { desc = "Save and Quit" })
+vim.keymap.set("n", "<leader>ss", ":w<CR>", { desc = "Save File" })
+vim.keymap.set("n", "<leader>sa", ":wa<CR>", { desc = "Save All Files" })
+vim.keymap.set("n", "<leader>sq", ":wq<CR>", { desc = "Save and Quit" })
 vim.keymap.set("n", "<leader>qq", ":q<CR>", { desc = "Quit" })
 
 -- Buffer Management
@@ -23,7 +24,7 @@ vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Focus Lower Window" })
 vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Focus Upper Window" })
 
 -- Errors
-vim.keymap.set("n", "<leader>ve", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Toggle Trouble" })
+vim.keymap.set("n", "<leader>ee", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Toggle Trouble" })
 vim.keymap.set("n", "<leader>en", ":lua vim.diagnostic.goto_next()<CR>", { desc = "Next Diagnostic" })
 vim.keymap.set("n", "<leader>ep", ":lua vim.diagnostic.goto_prev()<CR>", { desc = "Previous Diagnostic" })
 

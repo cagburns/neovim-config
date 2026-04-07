76651998a41fe7fa7ae77d495f9249e9599367b8
cagburns <callum.burns0131@gmail.com>
2026-04-07 23:13:55 +0100

    add easy-dotnet and migrate from nvim-lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,124 +1,99 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  config = function()
-    require("mason").setup({
-      registries = {
-        "github:mason-org/mason-registry",
-        "github:Crashdummyy/mason-registry",
-      },
-    })
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "ts_ls",
-        "angularls",
-        "html",
-        "cssls",
-        "eslint",
-        "lua_ls",
-        "rust_analyzer",
-      },
-      automatic_installation = true,
-    })
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			registries = {
+				"github:mason-org/mason-registry",
+				"github:Crashdummyy/mason-registry",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig", -- kept only for server default configs
+		dependencies = { "hrsh7th/cmp-nvim-lsp" },
+		config = function()
+			-- Keymaps: attach to every LSP buffer via the new autocmd API
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspAttach", { clear = true }),
+				callback = function(ev)
+					local opts = { buffer = ev.buf }
+					vim.keymap.set(
+						"n",
+						"gd",
+						vim.lsp.buf.definition,
+						vim.tbl_extend("force", opts, { desc = "Go to Definition" })
+					)
+					vim.keymap.set(
+						"n",
+						"gr",
+						vim.lsp.buf.references,
+						vim.tbl_extend("force", opts, { desc = "Go to References" })
+					)
+					vim.keymap.set(
+						"n",
+						"gi",
+						vim.lsp.buf.implementation,
+						vim.tbl_extend("force", opts, { desc = "Go to Implementation" })
+					)
+					vim.keymap.set(
+						"n",
+						"K",
+						vim.lsp.buf.hover,
+						vim.tbl_extend("force", opts, { desc = "Hover Documentation" })
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>rn",
+						vim.lsp.buf.rename,
+						vim.tbl_extend("force", opts, { desc = "Rename Symbol" })
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>ca",
+						vim.lsp.buf.code_action,
+						vim.tbl_extend("force", opts, { desc = "Code Action" })
+					)
+				end,
+			})
 
-    local on_attach = function(_, bufnr)
-      local opts = { buffer = bufnr }
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts, { desc = "Go to Definition" })
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts, { desc = "Go to References" })
-      vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts, { desc = "Go to Implementation" })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts, { desc = "Hover Documentation" })
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts, { desc = "Rename Symbol" })
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts, { desc = "Code Action" })
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts, { desc = "Go to Previous Diagnostic" })
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts, { desc = "Go to Next Diagnostic" })
-    end
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    -- Capabilities for cmp
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    vim.lsp.config("csharp_ls", {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
+			-- Servers that need no special config
+			local servers = { "ts_ls", "angularls", "html", "cssls", "rust_analyzer" }
+			for _, server in ipairs(servers) do
+				vim.lsp.config(server, { capabilities = capabilities })
+			end
 
-    -- TypeScript/JavaScript
-    vim.lsp.config("ts_ls", {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
+			-- ESLint: disable formatting so conform.nvim stays in charge
+			vim.lsp.config("eslint", {
+				capabilities = capabilities,
+				on_attach = function(client, _)
+					client.server_capabilities.documentFormattingProvider = false
+					client.server_capabilities.documentRangeFormattingProvider = false
+				end,
+				settings = {
+					workingDirectories = { mode = "auto" },
+					experimental = { useFlatConfig = nil },
+				},
+			})
 
-    -- Angular
-    vim.lsp.config("angularls", {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
+			-- Lua: tell the server "vim" is a valid global
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = { diagnostics = { globals = { "vim" } } },
+				},
+			})
 
-    -- HTML
-    vim.lsp.config("html", {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
-
-    -- CSS
-    vim.lsp.config("cssls", {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
-
-    -- ESLint
-    vim.lsp.config("eslint", {
-      on_attach = function(client, bufnr)
-        -- Disable ESLint formatting in favor of conform.nvim
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-        on_attach(client, bufnr)
-      end,
-      capabilities = capabilities,
-      settings = {
-        workingDirectories = { mode = "auto" },
-        experimental = {
-          useFlatConfig = nil,
-        },
-      },
-      -- root_dir = function(fname)
-      --   local util = require("lspconfig.util")
-      --   return util.root_pattern(
-      --     ".eslintrc",
-      --     ".eslintrc.js",
-      --     ".eslintrc.json",
-      --     ".eslintrc.cjs",
-      --     "eslint.config.js",
-      --     "eslint.config.mjs",
-      --     "eslint.config.cjs"
-      --   )(fname) or util.find_git_ancestor(fname)
-      -- end,
-    })
-
-    vim.lsp.config("roslyn", {
-      on_attach = on_attach,
-      settings = {
-        ["csharp|inlay_hints"] = {
-          csharp_enable_inlay_hints_for_implicit_object_creation = true,
-          csharp_enable_inlay_hints_for_implicit_variable_types = true,
-        },
-        ["csharp|code_lens"] = {
-          dotnet_enable_references_code_lens = true,
-        },
-      },
-    })
-
-    vim.lsp.config("lua_ls", {
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-        },
-      },
-    })
-    vim.lsp.enable({ "ts_ls", "angularls", "html", "cssls", "eslint", "lua_ls", "roslyn", "rust_analyzer" })
-  end,
+			vim.lsp.enable({
+				"ts_ls",
+				"angularls",
+				"html",
+				"cssls",
+				"eslint",
+				"lua_ls",
+				"roslyn",
+				"rust_analyzer",
+			})
+		end,
+	},
 }
